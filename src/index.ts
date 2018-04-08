a1b14af6ed9df24c1b385b46df77c768320a1028
yumetodo <yume-wikijp@live.jp>
2018-04-09 00:29:59 +0900

    chore: add tmp test function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { SheetService } from './sheet.service';
+/// <reference types="typescript" />
+import * as type from 'object-type-traits';
 
 declare var global: any;
 
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
+global.test_sheet_function = (): string => 'arikitari_na_sekai';
+
+global.test_sheet_function2 = (arg: any): number => {
+  function impl(n: number): number;
+  function impl(n?: any) {
+    if (type.of('Number') === n) {
+      return n + 1;
+    } else {
+      return 0;
+    }
+  }
+  return impl(arg);
 };
